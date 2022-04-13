e753ce779ea2a8f221a96f9b0fb9a72028177942
Dmitriy Khramov <thedcm@yandex.com>
2022-04-14 00:11:26 +0300

    Добавить веб-страницы

---
+++ Aiva/Views/Home/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aiva.Views.Home
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
